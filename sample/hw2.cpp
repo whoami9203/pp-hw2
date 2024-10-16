baecf9b57440464d72db0613e0357bc0fad4eefb
whoami9203 <39827915+whoami9203@users.noreply.github.com>
2024-10-16 20:30:52 +0800

    batch size = 32

--- sample/hw2.cpp
+++ sample/hw2.cpp
@@ -46,7 +46,7 @@ vec3 target_pos;  // target position in 3D space (x, y, z)
 unsigned char* raw_image;  // 1D image
 unsigned char** image;     // 2D image
 unsigned char* final_image;
-int batch_size = 16;
+int batch_size = 32;
 vec3 ro;    // ray (camera) origin
 vec3 ta;    // target position
 vec3 cf;    // forward vector
